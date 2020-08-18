23e811efed11e80208a9ee43ce3e459ce0a537e2
sabine stuck <sabine.moreira95@gmail.com>
2020-08-18 12:37:46 +0200

    Controller Igloo fait et marche, reste Seed a refaire

--- app/controllers/igloos_controller.rb
+++ app/controllers/igloos_controller.rb
@@ -1,36 +1,50 @@
 class IgloosController < ApplicationController
+# skip_before_action :authenticate_user!, only: [:home :show]
 
   def index
     @igloos = Igloo.all
   end
 
   def show
-
     @igloo = Igloo.find(params[:id])
   end
 
   def new
     @igloo = Igloo.new
+    @user = current_user
   end
 
   def create
     @igloo = Igloo.new(igloo_params)
-    @igloo.user = current_user
-    @igloo.save
+    @user = current_user
+    @igloo.user = @user
+    if @igloo.save
     redirect_to igloo_path(@igloo)
+    else
+      render :new
+    end
   end
 
   def edit
     @igloo = Igloo.find(params[:id])
+    unless current_user == @igloo.user
+      redirect_to(@igloo, alert: "Sorry, you cannot edit this igloo")
+    end
   end
 
   def update
     @igloo = Igloo.find(params[:id])
     @igloo.update(igloo_params)
+    if @igloo.save
+      redirect_to igloo_path(@igloo)
+    else
+      render :new
+    end
   end
 
   def destroy
     @igloo = Igloo.find(params[:id])
+    # @igloo = @booking.igloo
     @igloo.destroy
     redirect_to igloos_path(@igloo)
   end
--- db/seeds.rb
+++ db/seeds.rb
@@ -1,27 +1,8 @@
-# This file should contain all the record creation needed to seed the database with its default values.
-# The data can then be loaded with the rails db:seed command (or created alongside the database with db:setup).
-#
-# Examples:
-#
-#   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
-#   Character.create(name: 'Luke', movie: movies.first)
+require "Faker"
 
-Igloo.destroy_all
-
-igloos = Igloo.create! ([
-    { name: "Ice Casa", address: "vallée du fort, 73400 Tignes", description: "chaleur et fraicheur" },
-    { name: "Cold Water", address: "domaine de la vie, 73400 Tignes", description: "non mais à l'eau quoi !" },
-    { name: "Fresh Savour", address: "impasse Sixtine, 73500 Combloux", description: "au frais" },
-    { name: "Snow White", address: "lieu-dit de la montagne, 73945 Bourg Saint Maurice", description: "lieu zen et paisible" }
-])
-
-p "Created #{Igloo.name}"
-
-
-
-users = User.create! ([
-    { first_name: "Alexandre", last_name: "Sinn", email: "alex77@gmail.com", address: "2 rue du bal, 59000 Lille", password: "alexandre" },
-    { first_name: "Cecile", last_name: "Tarn", email: "cecile@gmail.com", address: "8 rue de bretagne, 59160 Lomme", password: "cecile" },
-])
-
-p "Created #{User.name}"
+5.times do
+    user = User.create!(email: Faker::Internet.email,
+                first_name: Faker::Name.first_name,
+                last_name: Faker::Name.last_name,
+                password: '123456')
+end
