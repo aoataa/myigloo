c63dc08fc2c9d4d0c52ae0ac8b6dfc258450ef3a
Amandyne Verdonck <amandyne.verdonck@gmail.com>
2020-08-18 15:31:30 +0200

    Add env.

--- db/seeds.rb
+++ db/seeds.rb
@@ -1,8 +1,48 @@
-require "Faker"
 
-5.times do
-    user = User.create!(email: Faker::Internet.email,
-                first_name: Faker::Name.first_name,
-                last_name: Faker::Name.last_name,
-                password: '123456')
-end
+require "open-uri"
+
+  user_one = User.new(
+      first_name: "Aurelien",
+      last_name: "Max",
+      email: "aurelien@lewagon.org",
+      password: '123456')
+  user_one.save
+
+  user_two = User.new(
+      first_name: "Sabine",
+      last_name: "Stuck",
+      email: "sabine@lewagon.org",
+      password: '123456')
+
+  user_two.save
+
+    file = URI.open('https://images.unsplash.com/photo-1548278651-843b1d7431a9?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=1350&q=80')
+    igloo_one = Igloo.new(
+      name: "Igloo Snow white",
+      address: "Tignes",
+      description: Faker::Lorem.paragraph,
+      user: user_one)
+    igloo_one.photo.attach(io: file, filename: 'nes.png', content_type: 'image/png')
+    igloo_one.save
+
+    igloo_two = Igloo.new(
+      name: "Igloo Ice",
+      address: "Val d'isère",
+      description: Faker::Lorem.paragraph,
+      user: user_one)
+    igloo_two.save
+
+    igloo_tree = Igloo.new(
+      name: "Icy Igloo",
+      address: "Vars",
+      description: Faker::Lorem.paragraph,
+      user: user_two)
+    igloo_tree.save
+
+    igloo_four = Igloo.new(
+      name: "Blue Igloo",
+      address: "Val Thorens",
+      description: Faker::Lorem.paragraph,
+      user: user_two)
+    igloo_four.save
+
