a62179aa586db5b104173222395487df9a1fe304
aurelien rothan <rothan.aurelien@gmail.com>
2020-08-18 14:24:31 +0200

    create_bookings

--- app/controllers/bookings_controller.rb
+++ app/controllers/bookings_controller.rb
@@ -1,24 +1,30 @@
 class BookingsController < ApplicationController
+    
+    def index
+        @bookings = booking.all
+    end
 
-
-    def new 
-        @user = Users.find(params[:user_id]
+    def new         
+        @igloo = Igloo.find(params[:igloo_id]
         @booking = Booking.new
     end 
-    
 
+    def create
+        @booking = Booking.new(booking_params)
+        # @booking_user = current_user
+        @user = User.find(params[:user_id])
+        @booking.user = @user
+        if booking.save
+            redirect_to booking_path(@booking)
+        else
+            flash[:alert] = "Please provide valid dates"
+            redirect_to igloo_path(@igloo)
+        end
+    end
 
+    private 
 
-
-
-
-
-
-
-
-
-
-
-
-
+    def booking_params
+        params.require(:booking).permit(:check_in, :check_out, :igloo_id)
+    end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -2,5 +2,6 @@ Rails.application.routes.draw do
   devise_for :users
   root to: 'pages#home'
   resources :igloos
+  resources :bookings, only: [:index, :new, :create]
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
 end
