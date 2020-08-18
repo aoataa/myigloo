5fe32d729cb45d089857bd1a709f1c92b0606d90
aurelien rothan <rothan.aurelien@gmail.com>
2020-08-18 17:58:19 +0200

    pundit_booking_suite

--- config/routes.rb
+++ config/routes.rb
@@ -1,13 +1,8 @@
 Rails.application.routes.draw do
   devise_for :users
   root to: 'pages#home'
-<<<<<<< HEAD
+
   resources :igloos do
-    resources :bookings, only: [:index, :new, :create]
+    resources :bookings, only: [:index, :show, :new, :create]
   end
-=======
-  resources :igloos
-  resources :bookings, only: [:index, :show, :new, :create]
->>>>>>> master
-  # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
 end
