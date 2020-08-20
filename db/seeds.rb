4980bf8a90c302326033fcadd0486074b3244cb7
sabine stuck <sabine.moreira95@gmail.com>
2020-08-20 15:45:17 +0200

    J'ai ajouté le prix et le rating sur Seed

--- db/seeds.rb
+++ db/seeds.rb
@@ -27,7 +27,7 @@ puts " create users and igloos"
 
     file = URI.open('https://images.unsplash.com/photo-1548278651-843b1d7431a9?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=1350&q=80')
     igloo_one = Igloo.new(
-      name: "Igloo Snow white",
+      name: "Igloo Snow White",
       address: "Val Claret, Tignes",
       description: "Vivez comme les esquimaux dans la plus simple des sérénités :
       dormez sur des couvertures à même la glace et profitez d'une 
@@ -35,6 +35,8 @@ puts " create users and igloos"
       seul au monde. Appréciez enfin une balade dans la neige, en raquette
       ou en luge fabriquée en matière 100% naturelle.
       Un moment unique qui restera gravé dans votre esprit.",
+      price: 199,
+      rating: 4,
       user: user_one)
     igloo_one.photos.attach(io: file, filename: 'nes.png', content_type: 'image/png')
     igloo_one.save
@@ -49,6 +51,8 @@ puts " create users and igloos"
       de tester l'elixir de vie à travers le froid pour retrouver
       une 2è nature. Vous aurez à disposition tout le nécessaire pour 
       apprécier votre séjour, nuit comme jour.",
+      price: 127,
+      rating: 4,
       user: user_one)
     igloo_two.photos.attach(io: file, filename: 'nes.png', content_type: 'image/png')
     igloo_two.save
@@ -56,12 +60,14 @@ puts " create users and igloos"
     file = URI.open('https://images.unsplash.com/photo-1587500919989-e6954bd42c95?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=1350&q=80')
     igloo_tree = Igloo.new(
       name: "Eyes Igloo",
-      address: "route des chalets, Méribel",
+      address: "Route des Chalets, Méribel",
       description: "Dans cet igloo, vous serez au plus proche de la nature. Entièrement
       conçu autour de son espace naturel et biodégradable selon les aléas de Mère Nature, 
       soyez surpris par sa force, elle vous laissera dormir en tout quiétude ou vous reveillera
       par sa colère. 
       Dépaysement et etonnement garantis",
+      price: 185,
+      rating: 5,
       user: user_two)
     igloo_tree.photos.attach(io: file, filename: 'nes.png', content_type: 'image/png')
     igloo_tree.save
@@ -74,6 +80,8 @@ puts " create users and igloos"
       Soyez au contact même de la nautre, de sa générosité ou aussi
       des ses caprices selon ses envies. Vous finirez par dormir à la belle étoile...
       peut-être :)",
+      price: 129,
+      rating: 3,
       user: user_two)
     igloo_four.photos.attach(io: file, filename: 'nes.png', content_type: 'image/png')
     igloo_four.save
@@ -86,6 +94,8 @@ puts " create users and igloos"
       vous faire une grande restauration, pas de quoi prendre un bain
       chaud durant 45 minutes. Mais de quoi en prendre plein les yeux 
       pendant une ou plusieurs nuits.",
+      price: 260,
+      rating: 5,
       user: user_two)
     igloo_five.photos.attach(io: file, filename: 'nes.png', content_type: 'image/png')
     igloo_five.save
@@ -94,9 +104,11 @@ puts " create users and igloos"
     igloo_six = Igloo.new(
       name: "Night Igloo",
       address: "Le Bonnet, Plagne Bellecote",
-      description: "entre eau et neige, une superbe situation pour un moment 
+      description: "Entre eau et neige, une superbe situation pour un moment 
       hors du commun ! vous vous sentirez comme chez vous sous ce magnifique manteau neigeux
       qui habille cet igloo unique",
+      price: 220,
+      rating: 5,
       user: user_two)
     igloo_six.photos.attach(io: file, filename: 'nes.png', content_type: 'image/png')
     igloo_six.save
@@ -105,10 +117,12 @@ puts " create users and igloos"
     igloo_seven = Igloo.new(
       name: "L'igloo du hameau",
       address: "Le Hameau, Flaine",
-      description: "unique aventure au sein du domaine de Flaine, 
+      description: "Unique aventure au sein du domaine de Flaine, 
       dans un igloo à même les pistes avec tout le nécessaire pour vivre 
       une nuit exceptionnelle avant de chausser les skis pour une journée 
       de plaisir !",
+      price: 227,
+      rating: 5,
       user: user_one)
     igloo_seven.photos.attach(io: file, filename: 'nes.png', content_type: 'image/png')
     igloo_seven.save
@@ -116,10 +130,12 @@ puts " create users and igloos"
     file = URI.open('https://images.unsplash.com/photo-1594157172900-ade6d2888cd1?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=500&q=60')
     igloo_eight = Igloo.new(
       name: "Dream igloo",
-      address: "Route du plan du vah, Courchevel",
+      address: "Route du Plan du Vah, Courchevel",
       description: "Le rêve pour une nuit inimaginable avant d'avoir tenté 
       l'expérience. Un domaine magique pour une nuit féérique qui n'attend plus que vous. 
       Testez vite pour avoir des étoiles plein les yeux dans ce cadre unique.",
+      price: 320,
+      rating: 4,
       user: user_one)
     igloo_eight.photos.attach(io: file, filename: 'nes.png', content_type: 'image/png')
     igloo_eight.save
